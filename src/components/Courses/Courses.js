a0b0ee25d7c370b2f1e3c3f79baaf5be4b552930
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-01-26 23:31:27 +0200

    fix issue

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,15 +1,13 @@
-import React, {
-  useState,
-  useEffect
-} from "react";
-import CourseCard from "../UI/CourseCard";
+import React, { useState, useEffect } from 'react';
+import CourseCard from '../UI/CourseCard';
 
 const Courses = () => {
   const [cours, setCours] = useState();
   const myData = async () => {
     const response = await fetch(
-      "https://tutorial.tarekmadady.com/track/allfree", {
-        method: "GET",
+      'https://tutorial.tarekmadady.com/track/allfree',
+      {
+        method: 'GET',
       }
     );
     const data = await response.json();
@@ -19,39 +17,26 @@ const Courses = () => {
   useEffect(() => {
     myData();
   }, []);
-  return ( <
-    section id = "popular-courses"
-    className = "courses"
-    dir = "rtl" >
-    <
-    div className = "container"
-    data - aos = "fade-up" >
-    <
-    div className = "section-title" > {
-      /* <h2>مسارات</h2> */ } <
-    p > المديولات التعليمية < /p> <
-    /div> <
-    div className = "row"
-    data - aos = "zoom-in"
-    data - aos - delay = "100" > {
-      cours &&
-      cours.map((m) => ( <
-        CourseCard key = {
-          m._id
-        }
-        tname = {
-          m.trackName
-        }
-        tabout = {
-          m.About_track
-        }
-        />
-      ))
-    } {
-      /* <CourseCard /> */ } <
-    /div> <
-    /div> <
-    /section>
+  return (
+    <section id="popular-courses" className="courses" dir="rtl">
+      <div className="container" data-aos="fade-up">
+        <div className="section-title">
+          {/* <h2>مسارات</h2> */}
+          <p> المديولات التعليمية </p>
+        </div>
+        <div className="row" data-aos="zoom-in" data-aos-delay="100">
+          {cours &&
+            cours.map((m) => (
+              <CourseCard
+                key={m._id}
+                tname={m.trackName}
+                tabout={m.About_track}
+              />
+            ))}
+          {/* <CourseCard /> */}
+        </div>
+      </div>
+    </section>
   );
 };
 
